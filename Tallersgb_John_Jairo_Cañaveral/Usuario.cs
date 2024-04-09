43234275355dcedf441bf394713737b454b71453
Jairocanaveral23 <161511593+Jairocanaveral23@users.noreply.github.com>
2024-04-09 12:27:11 -0500

    completed
    Este proyecto consiste en un programa de consola desarrollado en C# que permite gestionar préstamos de libros y usuarios de una biblioteca. La aplicación proporciona funcionalidades básicas para administrar el inventario de libros, así como también para manejar la información de los usuarios y los prestamos realizados.

---
+++ Tallersgb_John_Jairo_Cañaveral/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tallersgb_John_Jairo_Cañaveral
+{
+   public class Usuario
+    {
+        public Usuario(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+        public string Nombre { get; set; }
+
+
+        
+
+    }
+}
